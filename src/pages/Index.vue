a6041a2a3f6f48fc3c737301aadf9d28d497985b
bigzhu <vermiliondun@gmail.com>
2019-08-01 11:36:45 +0800

    基础

---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="column">
+        <div class="col">
+          <img alt="English buoy logo" src="~assets/english-buoy-logo.png" width="256" height="256">
+        </div>
+        <div class="col">
+          <Oauth :oauths="oauths" />
+        </div>
+    </div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+
+import Oauth from 'bz-q-lib/src/components/Oauth'
+export default {
+  components: {
+    Oauth
+  },
+  data: function () {
+    return {
+      oauths: [
+        {
+          type: 'google',
+          url: '/api/google'
+        }
+      ]
+    }
+  },
+  name: 'PageIndex'
+}
+</script>
